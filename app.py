b5c4d188a828d73bba883f63bedf8cc0f0dd27fe
Trishapatel09 <trishapatel1970@gmail.com>
2025-04-22 16:12:34 +0000

    added the flask project

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+  return"Hello Trisha!"
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
